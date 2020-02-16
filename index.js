ba7427479a57398ff8310477a4e6e87aab7c370e
sabasharat-ali <57292228+sabasharat-ali@users.noreply.github.com>
2020-02-16 15:32:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let myName='Joker'
+let profession='Terrorism'
+
+const result=`My name is ${myName} and my profession is ${profession}`;
+console.log(result);
+
+let paragraph=
+`bla bla lba blaflkjahdfkjh
+
+bla bla lba blaflkjahdfkjh
+bla bla lba blaflkjahdfkjh
+bla bla lba blaflkjahdfkjh
+bla bla lba blaflkjahdfkjh
+bla bla lba blaflkjahdfkjh
+bla bla lba blaflkjahdfkjh
+bla bla lba blaflkjahdfkjh
+bla bla lba blaflkjahdfkjh
+bla bla lba blaflkjahdfkjh
+bla bla lba blaflkjahdfkjh
+bla bla lba blaflkjahdfkjh
+bla bla lba blaflkjahdfkjh
+
+`
+console.log(paragraph); //WE CAN ASSIGN A WHOLE PARAGRAPH WITH THIS TOO
